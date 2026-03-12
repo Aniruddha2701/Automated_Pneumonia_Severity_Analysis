f61270652227da4b8896d22a73c4b33ed307c43a
Aniruddha2701 <priyanshumaurya7052@gmail.com>
2026-03-12 13:56:00 +0530

    Syntax error again but final solve.

--- preprocessing/transforms.py
+++ preprocessing/transforms.py
@@ -5,15 +5,19 @@ from PIL import Image
 
 
 # =========================
-# CLAHE FUNCTION
+# CLAHE TRANSFORM
 # =========================
+
 class CLAHETransform:
 
-    def__init__(self, clip_limit=2.0, tile_grid_size=(8, 8)):
-        self.clahe= cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size
+    def __init__(self, clip_limit=2.0, tile_grid_size=(8, 8)):
+        self.clahe = cv2.createCLAHE(
+            clipLimit=clip_limit,
+            tileGridSize=tile_grid_size
         )
 
     def __call__(self, img):
+
         img = np.array(img)
 
         # Convert to grayscale if needed
@@ -22,10 +26,10 @@ class CLAHETransform:
 
         img = self.clahe.apply(img)
 
-        # Convert back to 3-channel
+        # Convert back to 3 channel
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-    return Image.fromarray(img)
+        return Image.fromarray(img)
 
 
 # =========================
@@ -34,20 +38,19 @@ class CLAHETransform:
 
 train_transform = transforms.Compose([
 
-    transform.Lambda(lambda img: img.convert("RGB")),
+    transforms.Lambda(lambda img: img.convert("RGB")),
 
     CLAHETransform(),
 
     transforms.Resize((224, 224)),
 
-    transform.RandomHoriziontalFlip(p = 0.5),
+    transforms.RandomHorizontalFlip(p=0.5),
 
     transforms.ToTensor(),
-    
+
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
-
     )
 ])
 
@@ -57,40 +60,21 @@ train_transform = transforms.Compose([
 # =========================
 
 val_test_transform = transforms.Compose([
-    
+
     transforms.Lambda(lambda img: img.convert("RGB")),
-    
+
     CLAHETransform(),
-    
+
     transforms.Resize((224, 224)),
 
     transforms.ToTensor(),
-    
+
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
     )
 ])
 
-# =========================
-# VALIDATION / TEST TRANSFORM
-# =========================
-
-val_test_transform = transforms.Compose([
-    
-    transforms.Lambda(lambda img: img.convert("RGB")),
-    
-    CLAHETransform(),
-    
-    transforms.Resize((224, 224)),
-
-    transforms.ToTensor(),
-    
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
-])
 
 if __name__ == "__main__":
     print("Transforms module loaded successfully.")
